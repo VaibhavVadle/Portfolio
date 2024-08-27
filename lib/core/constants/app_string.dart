11f3bcc931ddb25c1a7c02b1a2d0def69c1f8315
VaibhavVadle <vaibhavvadle09@gmail.com>
2024-08-27 21:40:55 +0530

    Resume Link Updated

--- lib/core/constants/app_string.dart
+++ lib/core/constants/app_string.dart
@@ -1,6 +1,6 @@
 class AppStrings {
   static const String resumeUrl =
-      "https://firebasestorage.googleapis.com/v0/b/vaibhav-vadle.appspot.com/o/resume%2FVaibhav's%20Resume.pdf?alt=media&token=86d1fccb-1707-4148-ba49-5cadcc62e410";
+      "https://firebasestorage.googleapis.com/v0/b/vaibhav-vadle.appspot.com/o/resume%2FVaibhav%20Resume.pdf?alt=media&token=01c60e86-5de2-4763-81ff-3967269744b5";
   static const String linkedInUrl =
       "https://www.linkedin.com/in/vaibhav-vadle-2a6135198/";
   static const String githubUrl = "https://github.com/VaibhavVadle";
